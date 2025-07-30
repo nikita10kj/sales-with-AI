285c35fcaacb2089dc6dea62938532a8f4ac25c3
Nikita Kapadiya <nikikapadiya97@gmail.com>
2025-07-30 15:44:28 +0530

    reminders ser again

--- generate_email/tasks.py
+++ generate_email/tasks.py
@@ -15,46 +15,46 @@ from users.models import CustomUser
 from .models import TargetAudience
 import redis
 
-@shared_task
-def send_reminder_email_task(id):
-
-    er = ReminderEmail.objects.get(id=id)
-    # print("Sending email to:", er.email)
-    sendReminderEmail(er)
-    er.sent = True
-    er.save()
-
-redis_client = redis.StrictRedis.from_url(settings.CELERY_BROKER_URL)
-
-@shared_task
-def send_reminders():
-    lock_key = "lock:send_reminders"
-    lock_expiry_seconds = 900  # 15 minutes
-
-    # Set lock key if not already set (NX = only set if not exists)
-    is_locked = redis_client.set(lock_key, "1", ex=lock_expiry_seconds, nx=True)
-
-    if not is_locked:
-        print("Reminder task already running on another instance. Skipping.")
-        return
-
-    try:
-        today = timezone.now().date()
-        if not np.is_busday(today.strftime('%Y-%m-%d')):
-            return
-
-        reminder_emails = ReminderEmail.objects.filter(send_at=today, sent=False)
-
-        for index, er in enumerate(reminder_emails):
-            delay_seconds = index * 120
-            if not er.sent and not er.sent_email.stop_reminder:
-                send_reminder_email_task.apply_async(
-                    args=[er.id],
-                    countdown=delay_seconds
-                )
-    finally:
-        # Optional: remove lock (or let it expire naturally)
-        redis_client.delete(lock_key)
+# @shared_task
+# def send_reminder_email_task(id):
+#
+#     er = ReminderEmail.objects.get(id=id)
+#     # print("Sending email to:", er.email)
+#     sendReminderEmail(er)
+#     er.sent = True
+#     er.save()
+#
+# redis_client = redis.StrictRedis.from_url(settings.CELERY_BROKER_URL)
+#
+# @shared_task
+# def send_reminders():
+#     lock_key = "lock:send_reminders"
+#     lock_expiry_seconds = 900  # 15 minutes
+#
+#     # Set lock key if not already set (NX = only set if not exists)
+#     is_locked = redis_client.set(lock_key, "1", ex=lock_expiry_seconds, nx=True)
+#
+#     if not is_locked:
+#         print("Reminder task already running on another instance. Skipping.")
+#         return
+#
+#     try:
+#         today = timezone.now().date()
+#         if not np.is_busday(today.strftime('%Y-%m-%d')):
+#             return
+#
+#         reminder_emails = ReminderEmail.objects.filter(send_at=today, sent=False)
+#
+#         for index, er in enumerate(reminder_emails):
+#             delay_seconds = index * 120
+#             if not er.sent and not er.sent_email.stop_reminder:
+#                 send_reminder_email_task.apply_async(
+#                     args=[er.id],
+#                     countdown=delay_seconds
+#                 )
+#     finally:
+#         # Optional: remove lock (or let it expire naturally)
+#         redis_client.delete(lock_key)
 
 # @shared_task
 # def send_reminders():
