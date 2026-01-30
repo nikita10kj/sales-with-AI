476b959f024a7378bc4b8fc15f41b0caa4b6c996
HemishPansuriya <hemishnandlal@gmail.com>
2026-01-30 15:55:52 +0530

    debug

--- saleswithai/settings.py
+++ saleswithai/settings.py
@@ -278,11 +278,15 @@ AUTH_USER_MODEL = 'users.CustomUser'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = '/home/site/wwwroot/static/staticfiles'
+STATIC_URL = "/static/"
+STATIC_ROOT = "/home/site/wwwroot/static/staticfiles"
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
